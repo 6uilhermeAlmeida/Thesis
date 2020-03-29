97d0518af0cbeec5c4e93c10e82fcea7304d829a
Guilherme Almeida <guilherme.abreu.almeida@gmail.com>
2020-03-29 05:44:27 +0100

    Added new database tests and bash scripts.

---
+++ benchmark/src/androidTest/java/com/example/benchmark/rxjava/network/RxJavaNetworkBenchmark.kt
@@ -0,0 +1,40 @@
+package com.example.benchmark.rxjava.network
+
+import androidx.benchmark.junit4.measureRepeated
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.benchmark.rxjava.RxJavaBenchmark
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class RxJavaNetworkBenchmark : RxJavaBenchmark() {
+
+    @Test
+    fun fetch_movies_single_request() = benchmarkRule.measureRepeated {
+        remoteSource.getTrendingMovies().blockingGet()
+    }
+
+    @Test
+    fun fetch_two_movie_details() = benchmarkRule.measureRepeated {
+        val moviesIds = List(2) { 1 }
+        repository.getMoviesDetail(moviesIds).blockingGet()
+    }
+
+    @Test
+    fun fetch_ten_movie_details() = benchmarkRule.measureRepeated {
+        val moviesIds = List(10) { 1 }
+        repository.getMoviesDetail(moviesIds).blockingGet()
+    }
+
+    @Test
+    fun fetch_twenty_movie_details() = benchmarkRule.measureRepeated {
+        val moviesIds = List(20) { 1 }
+        repository.getMoviesDetail(moviesIds).blockingGet()
+    }
+
+    @Test
+    fun fetch_fifty_movie_details() = benchmarkRule.measureRepeated {
+        val moviesIds = List(50) { 1 }
+        repository.getMoviesDetail(moviesIds).blockingGet()
+    }
+}
