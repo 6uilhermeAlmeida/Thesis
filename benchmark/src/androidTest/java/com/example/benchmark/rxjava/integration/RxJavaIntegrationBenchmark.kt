59bd813f0c26ea604b674bf17a3555a4678f5e27
Guilherme Almeida <guilherme.abreu.almeida@gmail.com>
2020-04-04 00:30:45 +0100

    Added new integration tests.

--- benchmark/src/androidTest/java/com/example/benchmark/rxjava/integration/RxJavaIntegrationBenchmark.kt
+++ benchmark/src/androidTest/java/com/example/benchmark/rxjava/integration/RxJavaIntegrationBenchmark.kt
@@ -3,6 +3,8 @@ package com.example.benchmark.rxjava.integration
 import androidx.benchmark.junit4.measureRepeated
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.benchmark.rxjava.RxJavaBenchmark
+import com.example.kitprotocol.transformer.toEntity
+import io.reactivex.Completable
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -10,7 +12,38 @@ import org.junit.runner.RunWith
 class RxJavaIntegrationBenchmark : RxJavaBenchmark() {
 
     @Test
-    fun fetch_movies_from_network_and_insert_in_db() = benchmarkRule.measureRepeated {
-        repository.fetchTrendingMovies().blockingAwait()
+    fun integration_1() = benchmarkRule.measureRepeated {
+        remoteSource.getTrendingMovies()
+            .flatMap { repository.getMoviesDetail(List(20) { it }) }
+            .flatMapCompletable { repository.insertMoviesToDatabase(it.mapNotNull { movieDetails -> movieDetails.toEntity() }) }
+            .blockingAwait()
+    }
+
+    @Test
+    fun integration_2() = benchmarkRule.measureRepeated {
+        remoteSource.getTrendingMovies()
+            .flatMap { remoteSource.getTrendingMovies() }
+            .flatMap { repository.getMoviesDetail(List(20) { it }) }
+            .flatMap { repository.getMoviesDetail(List(20) { it }) }
+            .flatMapCompletable {
+                repository.insertMoviesToDatabase(it.mapNotNull { movieDetails -> movieDetails.toEntity() })
+                    .andThen(repository.insertMoviesToDatabase(it.mapNotNull { movieDetails -> movieDetails.toEntity() }))
+            }.blockingAwait()
+    }
+
+    @Test
+    fun integration_3() = benchmarkRule.measureRepeated {
+        remoteSource.getTrendingMovies()
+            .flatMap { remoteSource.getTrendingMovies() }
+            .flatMap { remoteSource.getTrendingMovies() }
+            .flatMap { repository.getMoviesDetail(List(20) { it }) }
+            .flatMap { repository.getMoviesDetail(List(20) { it }) }
+            .flatMap { repository.getMoviesDetail(List(20) { it }) }
+            .flatMapCompletable {
+                repository.insertMoviesToDatabase(it.mapNotNull { movieDetails -> movieDetails.toEntity() })
+                    .andThen(repository.insertMoviesToDatabase(it.mapNotNull { movieDetails -> movieDetails.toEntity() }))
+                    .andThen(repository.insertMoviesToDatabase(it.mapNotNull { movieDetails -> movieDetails.toEntity() }))
+                    .andThen(Completable.complete())
+            }.blockingAwait()
     }
 }
