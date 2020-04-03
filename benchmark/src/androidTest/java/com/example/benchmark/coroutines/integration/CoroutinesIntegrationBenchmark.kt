5574c32618fdceda55027c21f82ff70408ded855
Guilherme Almeida <guilherme.abreu.almeida@gmail.com>
2020-04-04 00:30:45 +0100

    Added new integration tests.

--- benchmark/src/androidTest/java/com/example/benchmark/coroutines/integration/CoroutinesIntegrationBenchmark.kt
+++ benchmark/src/androidTest/java/com/example/benchmark/coroutines/integration/CoroutinesIntegrationBenchmark.kt
@@ -3,14 +3,60 @@ package com.example.benchmark.coroutines.integration
 import androidx.benchmark.junit4.measureRepeated
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.benchmark.coroutines.CoroutinesBenchmark
+import com.example.kitprotocol.transformer.toEntity
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class CoroutinesIntegrationBenchmark : CoroutinesBenchmark() {
+
     @Test
-    fun fetch_movies_from_network_and_insert_in_db() = benchmarkRule.measureRepeated {
-        runBlocking { repository.fetchTrendingMovies() }
+    fun integration_1() = benchmarkRule.measureRepeated {
+        runBlocking {
+            remoteSource.getTrendingMovies()
+            val movieDetails = with(repository) { getMoviesDetails(List(20) { it }).awaitAll() }
+            repository.insertMoviesToDatabase(movieDetails.mapNotNull { it.toEntity() })
+        }
+    }
+
+    @Test
+    fun integration_2() = benchmarkRule.measureRepeated {
+
+        runBlocking {
+
+            remoteSource.getTrendingMovies()
+            remoteSource.getTrendingMovies()
+
+            val movieDetails = with(repository) {
+                getMoviesDetails(List(20) { it }).awaitAll()
+                getMoviesDetails(List(20) { it }).awaitAll()
+            }
+
+            repository.insertMoviesToDatabase(movieDetails.mapNotNull { it.toEntity() })
+            repository.insertMoviesToDatabase(movieDetails.mapNotNull { it.toEntity() })
+        }
+    }
+
+    @Test
+    fun integration_3() = benchmarkRule.measureRepeated {
+
+        runBlocking {
+
+            remoteSource.getTrendingMovies()
+            remoteSource.getTrendingMovies()
+            remoteSource.getTrendingMovies()
+
+            val movieDetails = with(repository) {
+                getMoviesDetails(List(20) { it }).awaitAll()
+                getMoviesDetails(List(20) { it }).awaitAll()
+                getMoviesDetails(List(20) { it }).awaitAll()
+            }
+
+            repository.insertMoviesToDatabase(movieDetails.mapNotNull { it.toEntity() })
+            repository.insertMoviesToDatabase(movieDetails.mapNotNull { it.toEntity() })
+            repository.insertMoviesToDatabase(movieDetails.mapNotNull { it.toEntity() })
+        }
     }
 }
