b74f22854a110250606bf4db02cda4642401d739
Guilherme Almeida <guilherme.abreu.almeida@gmail.com>
2020-04-20 00:34:31 +0100

    Added new integration test. New gradle task for automatic benchmarking. Libs update

--- benchmark/src/androidTest/java/com/example/benchmark/rxjava/integration/RxJavaIntegrationBenchmark.kt
+++ benchmark/src/androidTest/java/com/example/benchmark/rxjava/integration/RxJavaIntegrationBenchmark.kt
@@ -3,10 +3,15 @@ package com.example.benchmark.rxjava.integration
 import androidx.benchmark.junit4.measureRepeated
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.benchmark.rxjava.RxJavaBenchmark
+import com.example.kitprotocol.rest.model.MovieResponse
 import com.example.kitprotocol.transformer.toEntity
+import io.reactivex.BackpressureStrategy
 import io.reactivex.Completable
+import io.reactivex.Flowable
+import io.reactivex.schedulers.Schedulers
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.concurrent.TimeUnit
 
 @RunWith(AndroidJUnit4::class)
 class RxJavaIntegrationBenchmark : RxJavaBenchmark() {
@@ -64,5 +69,17 @@ class RxJavaIntegrationBenchmark : RxJavaBenchmark() {
                     .andThen(repository.insertMoviesToDatabase(it.mapNotNull { movieDetails -> movieDetails.toEntity() }))
                     .andThen(Completable.complete())
             }.blockingAwait()
+    }
+
+    @Test
+    fun integration_reactive() = benchmarkRule.measureRepeated {
+
+        val flowable = Flowable.create<MovieResponse>({ emitter ->
+            emitter.onNext(remoteSource.getTrendingMovies().blockingGet())
+        }, BackpressureStrategy.LATEST)
+            .subscribeOn(Schedulers.io())
+            .delay(1, TimeUnit.SECONDS)
+
+        flowable.blockingFirst()
     }
 }
