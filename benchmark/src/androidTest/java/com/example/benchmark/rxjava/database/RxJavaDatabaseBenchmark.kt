0a852ac0b2dbdb807c52dc0a7d1db9bfe6b040f0
Guilherme Almeida <guilherme.abreu.almeida@gmail.com>
2020-04-23 23:34:05 +0100

    Added new integration test. Extracted tests to interfaces to keep consistent naming.

--- benchmark/src/androidTest/java/com/example/benchmark/rxjava/database/RxJavaDatabaseBenchmark.kt
+++ benchmark/src/androidTest/java/com/example/benchmark/rxjava/database/RxJavaDatabaseBenchmark.kt
@@ -2,6 +2,7 @@ package com.example.benchmark.rxjava.database
 
 import androidx.benchmark.junit4.measureRepeated
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.benchmark.IDatabaseBenchmark
 import com.example.benchmark.mock.getMockEntity
 import com.example.benchmark.rxjava.RxJavaBenchmark
 import io.reactivex.Single
@@ -12,7 +13,7 @@ import org.junit.runner.RunWith
 
 
 @RunWith(AndroidJUnit4::class)
-class RxJavaDatabaseBenchmark : RxJavaBenchmark() {
+class RxJavaDatabaseBenchmark : RxJavaBenchmark(), IDatabaseBenchmark {
 
     @Before
     fun before() {
@@ -24,37 +25,37 @@ class RxJavaDatabaseBenchmark : RxJavaBenchmark() {
      */
 
     @Test
-    fun insert_two_movies() = benchmarkRule.measureRepeated {
+    override fun insert_two_movies() = benchmarkRule.measureRepeated {
         val entities = List(2) { getMockEntity(it) }
         localSource.insertAsCompletable(entities).blockingAwait()
     }
 
     @Test
-    fun insert_ten_movies() = benchmarkRule.measureRepeated {
+    override fun insert_ten_movies() = benchmarkRule.measureRepeated {
         val entities = List(10) { getMockEntity(it) }
         localSource.insertAsCompletable(entities).blockingAwait()
     }
 
     @Test
-    fun insert_twenty_movies() = benchmarkRule.measureRepeated {
+    override fun insert_twenty_movies() = benchmarkRule.measureRepeated {
         val entities = List(20) { getMockEntity(it) }
         localSource.insertAsCompletable(entities).blockingAwait()
     }
 
     @Test
-    fun insert_fifty_movies() = benchmarkRule.measureRepeated {
+    override fun insert_fifty_movies() = benchmarkRule.measureRepeated {
         val entities = List(50) { getMockEntity(it) }
         localSource.insertAsCompletable(entities).blockingAwait()
     }
 
     @Test
-    fun insert_one_hundred_movies() = benchmarkRule.measureRepeated {
+    override fun insert_one_hundred_movies() = benchmarkRule.measureRepeated {
         val entities = List(100) { getMockEntity(it) }
         localSource.insertAsCompletable(entities).blockingAwait()
     }
 
     @Test
-    fun clear_and_insert_twenty_movies() = benchmarkRule.measureRepeated {
+    override fun clear_and_insert_twenty_movies() = benchmarkRule.measureRepeated {
         clearAndInsertMovies(20)
     }
 
@@ -63,25 +64,25 @@ class RxJavaDatabaseBenchmark : RxJavaBenchmark() {
      */
 
     @Test
-    fun query_twenty_movies() = benchmarkRule.measureRepeated {
+    override fun query_twenty_movies() = benchmarkRule.measureRepeated {
         runWithTimingDisabled { clearAndInsertMovies(size = 20) }
         localSource.allBySingle().blockingGet()
     }
 
     @Test
-    fun query_fifty_movies() = benchmarkRule.measureRepeated {
+    override fun query_fifty_movies() = benchmarkRule.measureRepeated {
         runWithTimingDisabled { clearAndInsertMovies(size = 50) }
         localSource.allBySingle().blockingGet()
     }
 
     @Test
-    fun query_one_hundred_movies() = benchmarkRule.measureRepeated {
+    override fun query_one_hundred_movies() = benchmarkRule.measureRepeated {
         runWithTimingDisabled { clearAndInsertMovies(size = 100) }
         localSource.allBySingle().blockingGet()
     }
 
     @Test
-    fun query_twenty_movies_in_parallel() = benchmarkRule.measureRepeated {
+    override fun query_twenty_movies_in_parallel() = benchmarkRule.measureRepeated {
 
         runWithTimingDisabled { clearAndInsertMovies(20) }
 
