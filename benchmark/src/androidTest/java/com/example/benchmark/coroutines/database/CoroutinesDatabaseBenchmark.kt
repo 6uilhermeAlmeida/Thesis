ac8b69974c886da66c4d308d7ee17ea76d3bef60
Guilherme Almeida <guilherme.abreu.almeida@gmail.com>
2020-04-26 23:23:59 +0100

    Overall code cleanup.

--- benchmark/src/androidTest/java/com/example/benchmark/coroutines/database/CoroutinesDatabaseBenchmark.kt
+++ benchmark/src/androidTest/java/com/example/benchmark/coroutines/database/CoroutinesDatabaseBenchmark.kt
@@ -8,7 +8,6 @@ import com.example.benchmark.mock.getMockEntity
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.runBlocking
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -16,17 +15,15 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class CoroutinesDatabaseBenchmark : CoroutinesBenchmark(), IDatabaseBenchmark {
 
-    @Before
-    fun before() {
-        runBlocking { localSource.suspendNuke() }
-    }
-
     /**
      * Inserts
      */
 
     @Test
     override fun insert_two_movies() = benchmarkRule.measureRepeated {
+        runWithTimingDisabled {
+            runBlocking { localSource.suspendNuke() }
+        }
         runBlocking {
             val movieEntities = List(2) { getMockEntity(it) }
             localSource.suspendInsert(movieEntities)
@@ -35,6 +32,9 @@ class CoroutinesDatabaseBenchmark : CoroutinesBenchmark(), IDatabaseBenchmark {
 
     @Test
     override fun insert_ten_movies() = benchmarkRule.measureRepeated {
+        runWithTimingDisabled {
+            runBlocking { localSource.suspendNuke() }
+        }
         runBlocking {
             val movieEntities = List(10) { getMockEntity(it) }
             localSource.suspendInsert(movieEntities)
@@ -43,6 +43,9 @@ class CoroutinesDatabaseBenchmark : CoroutinesBenchmark(), IDatabaseBenchmark {
 
     @Test
     override fun insert_twenty_movies() = benchmarkRule.measureRepeated {
+        runWithTimingDisabled {
+            runBlocking { localSource.suspendNuke() }
+        }
         runBlocking {
             val movieEntities = List(20) { getMockEntity(it) }
             localSource.suspendInsert(movieEntities)
@@ -51,6 +54,9 @@ class CoroutinesDatabaseBenchmark : CoroutinesBenchmark(), IDatabaseBenchmark {
 
     @Test
     override fun insert_fifty_movies() = benchmarkRule.measureRepeated {
+        runWithTimingDisabled {
+            runBlocking { localSource.suspendNuke() }
+        }
         runBlocking {
             val movieEntities = List(50) { getMockEntity(it) }
             localSource.suspendInsert(movieEntities)
@@ -59,6 +65,9 @@ class CoroutinesDatabaseBenchmark : CoroutinesBenchmark(), IDatabaseBenchmark {
 
     @Test
     override fun insert_one_hundred_movies() = benchmarkRule.measureRepeated {
+        runWithTimingDisabled {
+            runBlocking { localSource.suspendNuke() }
+        }
         runBlocking {
             val movieEntities = List(100) { getMockEntity(it) }
             localSource.suspendInsert(movieEntities)
