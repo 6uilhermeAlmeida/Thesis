d4da0755d7a7979678fdc5d35c90517a11de83a8
Guilherme Almeida <guilherme.abreu.almeida@gmail.com>
2020-08-04 02:00:04 +0100

    Normalizing db access for RxJava

--- rxjavakit/src/main/java/com/example/rxjavakit/location/AddressesSingle.kt
+++ rxjavakit/src/main/java/com/example/rxjavakit/location/AddressesSingle.kt
@@ -8,8 +8,11 @@ import io.reactivex.Single
 internal fun getAddresses(addressRepository: AddressRepository, location: Location, maxResults: Int) =
     Single.create<List<Address>> { emitter ->
         addressRepository.getAddresses(location.longitude, location.latitude, maxResults) { addresses, throwable ->
-            addresses?.let { emitter.onSuccess(it) }
-                ?: throwable?.let { emitter.onError(it) }
-                ?: emitter.onError(IllegalStateException("At least one value must be not null"))
+            if (addresses != null) {
+                emitter.onSuccess(addresses)
+            } else {
+                val exception = checkNotNull(throwable) { "If the result is null, the exception cannot be." }
+                emitter.onError(exception)
+            }
         }
     }
